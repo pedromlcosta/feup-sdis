b918752f31e7831d96ab7b1bde2c4ca1c653f844
Ncf16 <joaofilipe26@gmail.com>
2016-03-10 10:43:48 +0000

    Changed main argument

--- SDIS_PROJ1/src/main/Main.java
+++ SDIS_PROJ1/src/main/Main.java
@@ -4,7 +4,7 @@ import file.FileID;
 
 public class Main {
 	public static void main(String args[]) {
-		//System.out.println(Extra.SHA256("HI"));
-		FileID f= new FileID("C:\\Users\\Filipe-PC\\Downloads\\Documents\\New.txt");
+		// System.out.println(Extra.SHA256("HI"));
+		FileID f = new FileID("");
 	}
 }
