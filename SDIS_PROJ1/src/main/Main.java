4bedb0db6651e2db3ef231fb2365b50af0422984
Ncf16 <joaofilipe26@gmail.com>
2016-03-27 23:40:37 +0100

    corrigir testing bug

--- SDIS_PROJ1/src/main/Main.java
+++ SDIS_PROJ1/src/main/Main.java
@@ -7,7 +7,6 @@ public class Main {
 	public static void main(String arg[]) throws IOException, ClassNotFoundException {
 
 		System.out.println(System.getProperty("user.dir"));
-		new ServerThread().start();
 		System.out.println(System.getProperty("END"));
 	}
 }
