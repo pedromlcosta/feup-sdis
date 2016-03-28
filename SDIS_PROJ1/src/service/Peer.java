b6cac472dce0fe3e8d71a6be0f204ac8bce3357d
pedromlcosta <pedromlcosta@outlook.pt>
2016-03-28 17:56:53 +0100

    a

--- SDIS_PROJ1/src/service/Peer.java
+++ SDIS_PROJ1/src/service/Peer.java
@@ -214,8 +214,8 @@ public class Peer implements Invocation {
 		// e os metodos para enviar para os canais que queremos as cenas
 
 		// Call backup protocol through dispatcher
-		//Thread backup = new BackupProtocol(filePath, desiredRepDegree, "1.0", Peer.getInstance());
-		//backup.start();
+		Thread backup = new BackupProtocol(filePath, desiredRepDegree, "1.0", Peer.getInstance());
+		backup.start();
 		System.out.println("backup called");
 		return "backup sent";
 	}
