932ca1cb2206ddc665e85ac29d190a5f15dd2c36
Ncf16 <joaofilipe26@gmail.com>
2016-04-26 11:20:36 +0100

    Adding TODO & some changes to Tracker who has the backup/active flag

--- SDIS_PROJ2/src/service/Tracker.java
+++ SDIS_PROJ2/src/service/Tracker.java
@@ -8,8 +8,11 @@ import channels.MulticastServer;
 public class Tracker {
 	private MulticastServer monitorConnection;
 	private MulticastServer peerConnection;
+	private Tracker backup;
+	private boolean backupFlag;
+	private boolean activeFlag;
 
-	public Tracker(InetAddress addrPeer, int portPeer, InetAddress addrMonitor, int portMonitor) throws IOException {
+	public Tracker(InetAddress addrPeer, int portPeer, InetAddress addrMonitor, int portMonitor, boolean backupFlag, boolean activeFlag) throws IOException {
 		super();
 		this.peerConnection = new MulticastServer(false, addrPeer, portPeer);
 		this.peerConnection.createSocket();
@@ -17,6 +20,8 @@ public class Tracker {
 		this.monitorConnection = new MulticastServer(false, addrMonitor, portMonitor);
 		this.monitorConnection.createSocket();
 		this.monitorConnection.joinMulticastGroup();
+		this.backupFlag = backupFlag;
+		this.activeFlag = activeFlag;
 
 	}
 
@@ -34,6 +39,30 @@ public class Tracker {
 
 	public void setPeerConnection(MulticastServer peerConnection) {
 		this.peerConnection = peerConnection;
+	}
+
+	public Tracker getBackup() {
+		return backup;
+	}
+
+	public void setBackup(Tracker backup) {
+		this.backup = backup;
+	}
+
+	public boolean isBackupFlag() {
+		return backupFlag;
+	}
+
+	public void setBackupFlag(boolean backupFlag) {
+		this.backupFlag = backupFlag;
+	}
+
+	public boolean isActiveFlag() {
+		return activeFlag;
+	}
+
+	public void setActiveFlag(boolean activeFlag) {
+		this.activeFlag = activeFlag;
 	}
 
 }
